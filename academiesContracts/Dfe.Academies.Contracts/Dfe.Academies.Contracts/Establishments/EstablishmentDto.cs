c893a190127ec305d18a1f97a7729b3099563e15
plockwood <paul.lockwood@education.gov.uk>
2023-10-17 14:41:07 +0100

    setup and initial contracts

---
+++ academiesContracts/Dfe.Academies.Contracts/Dfe.Academies.Contracts/Establishments/EstablishmentDto.cs
@@ -0,0 +1,75 @@
+﻿namespace Dfe.Academies.Contracts.Establishments;
+
+[Serializable]
+public class EstablishmentDto
+{
+
+    public string Urn { get; set; }
+    public string Name { get; set; }
+    public string LocalAuthorityCode { get; set; }
+    public string LocalAuthorityName { get; set; }
+    public string OfstedRating { get; set; }
+    public string OfstedLastInspection { get; set; }
+    public string StatutoryLowAge { get; set; }
+    public string StatutoryHighAge { get; set; }
+    public string SchoolCapacity { get; set; }
+    public string Pfi { get; set; }
+    public string EstablishmentNumber { get; set; }
+    public NameAndCodeDto Diocese { get; set; }
+    public NameAndCodeDto EstablishmentType { get; set; }
+    public NameAndCodeDto Gor { get; set; }
+    public NameAndCodeDto PhaseOfEducation { get; set; }
+    public NameAndCodeDto ReligiousCharacter { get; set; }
+    public NameAndCodeDto ParliamentaryConstituency { get; set; }
+    public CensusDto Census { get; set; }
+    public MisEstablishmentDto MISEstablishment { get; set; }
+    public AddressDto Address { get; set; }
+}
+
+[Serializable]
+public class NameAndCodeDto
+{
+    public string Name { get; set; }
+    public string Code { get; set; }
+}
+
+[Serializable]
+public class AddressDto
+{
+    public string Street { get; set; }
+
+    public string Town { get; set; }
+
+    public string County { get; set; }
+
+    public string Postcode { get; set; }
+
+    public string Locality { get; set; }
+
+    public string Additional { get; set; }
+
+}
+
+[Serializable]
+public class MisEstablishmentDto
+{
+    public string DateOfLatestSection8Inspection { get; set; }
+    public string InspectionEndDate { get; set; }
+
+    public string OverallEffectiveness { get; set; }
+    public string QualityOfEducation { get; set; }
+    public string BehaviourAndAttitudes { get; set; }
+    public string PersonalDevelopment { get; set; }
+    public string EffectivenessOfLeadershipAndManagement { get; set; }
+
+    public string EarlyYearsProvision { get; set; }
+    public string SixthFormProvision { get; set; }
+    public string Weblink { get; set; }
+}
+
+[Serializable]
+public class CensusDto
+{
+    public string NumberOfPupils { get; set; }
+    public string PercentageFsm { get; set; }
+}
