63d6926799c142ef86104ce7cea07e9394365847
plockwood <paul.lockwood@education.gov.uk>
2023-11-07 16:21:05 +0000

    folder structure change to coupling of contracts to api version and added pagedDataResponse so it can be shared with client

---
+++ academiesContracts/Dfe.Academies.Contracts/V4/PagedDataResponse.cs
@@ -0,0 +1,26 @@
+﻿namespace Dfe.Academies.Contracts.V4;
+using System.Collections.Generic;
+
+public class PagedDataResponse<TResponse> where TResponse : class
+{
+
+    public IEnumerable<TResponse> Data { get; set; }
+    public PagingResponse Paging { get; set; }
+
+    public PagedDataResponse() => Data = new List<TResponse>();
+
+    public PagedDataResponse(IEnumerable<TResponse> data, PagingResponse pagingResponse)
+    {
+        Data = data;
+        Paging = pagingResponse;
+    }
+
+    public PagedDataResponse(TResponse data) => Data = new List<TResponse> { data };
+
+}
+public class PagingResponse
+{
+    public int Page { get; set; }
+    public int RecordCount { get; set; }
+    public string NextPageUrl { get; set; }
+}
