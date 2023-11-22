7cc45ff00d5a155a3e4d6fde8271e817a481a494
plockwood <paul.lockwood@education.gov.uk>
2023-11-22 17:38:21 +0000

    Added name and code dto as the trust type

--- academiesContracts/Dfe.Academies.Contracts/V4/Trusts/TrustDto.cs
+++ academiesContracts/Dfe.Academies.Contracts/V4/Trusts/TrustDto.cs
@@ -1,12 +1,14 @@
 ﻿namespace Dfe.Academies.Contracts.V4.Trusts;
 
+using Dfe.Academies.Contracts.V4.Establishments;
+
 [Serializable]
 public class TrustDto
 {
     public string Name { get; set; }
 
     public string Ukprn { get; set; }
-    public string Type { get; set; }
+    public NameAndCodeDto Type { get; set; }
 
     public string CompaniesHouseNumber { get; set; }
 
