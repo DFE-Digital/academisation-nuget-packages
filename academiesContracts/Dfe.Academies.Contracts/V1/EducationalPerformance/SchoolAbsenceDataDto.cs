f86270447b1ff0c37b1aee9311979e6d31db2154
plockwood <paul.lockwood@education.gov.uk>
2024-01-11 11:17:55 +0000

    added the a contract for the absence data which has been added to the v1 educational performance  data

---
+++ academiesContracts/Dfe.Academies.Contracts/V1/EducationalPerformance/SchoolAbsenceDataDto.cs
@@ -0,0 +1,26 @@
+namespace Dfe.Academies.Contracts.V1.EducationalPerformance
+{
+    /// <summary>
+    /// Absence Data Response 
+    /// </summary>
+    public class SchoolAbsenceDataDto
+    {
+        /// <summary>
+        /// Acdemic Year
+        /// </summary>
+        public string Year { get; set; }
+
+        /// <summary>
+        ///Percentage of possible mornings or afternoons recorded as an absence from school for whatever reason,
+        ///whether authorised or unauthorised, across the full academic year.
+        /// </summary>
+        public string? OverallAbsence { get; set; }
+
+        /// <summary>
+        ///The percentage of pupils missing 10% or more of the mornings or afternoons they could attend, 
+        ///meaning that if a pupil’s overall rate of absence is 10% or higher across the full academic 
+        ///year they will be classified as persistently absent.
+        /// </summary>
+        public string? PersistentAbsence { get; set; }
+    }
+}
